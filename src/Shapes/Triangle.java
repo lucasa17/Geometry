2b0eae17861d89acf5aa350348684094f814dbc5
lucasa17 <134516774+lucasa17@users.noreply.github.com>
2025-08-19 08:41:50 -0300

    Update Triangle.java

--- src/Shapes/Triangle.java
+++ src/Shapes/Triangle.java
@@ -26,7 +26,7 @@ public class Triangle {
         this.heightSize = heightSizeTriangle;
     }
     
-    void Draw(){
+    void draw(){
     	int height = Math.round(this.heightSize);
     	for (int i = 1; i <= height; i++) {
     	    for (int j = 1; j <= i; j++) {
